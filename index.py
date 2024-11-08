b7b24df66c2ed572d3c1571abfbb9e476b20d4ab
GiovaniAppezzato <giovani.apztt@gmail.com>
2024-11-07 22:14:22 -0300

    feat: adicionado o script básico para a detecção das emoções usando o modelo FER

--- index.py
+++ index.py
@@ -1,15 +1,25 @@
 import cv2
+from fer import FER
+
+emotion_detector = FER()
 
 cap = cv2.VideoCapture(0)
 
-# Only show the 
 while True:
     ret, frame = cap.read()
+    if not ret:
+        break
 
-    # Show the frame
-    cv2.imshow('frame', frame)
-
-    # Break the loop
+    emotions = emotion_detector.detect_emotions(frame)
+    
+    for emotion in emotions:
+        box = emotion['box']
+        emotion_label = max(emotion['emotions'], key=emotion['emotions'].get)
+        
+        cv2.rectangle(frame, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]), (0, 255, 0), 2)
+        cv2.putText(frame, emotion_label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    
+    cv2.imshow('Emotions', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
